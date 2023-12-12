d91bac2392bacb0251756ebdef4d2dfbc409da50
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2023-12-12 01:20:20 +0100

    customer testsss

--- src/test/java/com/example/backendexam2023/Address/AddressServiceUnitTests.java
+++ src/test/java/com/example/backendexam2023/Address/AddressServiceUnitTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageRequest;
+
 
 import java.util.List;
 import java.util.Optional;
--- src/test/java/com/example/backendexam2023/Customer/CustomerServiceUnitTests.java
+++ src/test/java/com/example/backendexam2023/Customer/CustomerServiceUnitTests.java
@@ -1,7 +1,9 @@
 package com.example.backendexam2023.Customer;
 
+import com.example.backendexam2023.Model.Address.Address;
 import com.example.backendexam2023.Model.Customer.Customer;
 import com.example.backendexam2023.Records.OperationResult;
+import com.example.backendexam2023.Repository.AddressRepository;
 import com.example.backendexam2023.Repository.CustomerRepository;
 import com.example.backendexam2023.Service.CustomerService;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,9 @@ public class CustomerServiceUnitTests {
 
     @MockBean
     private CustomerRepository customerRepository;
+
+    @MockBean
+    private AddressRepository addressRepository;
 
     @Autowired
     private CustomerService customerService;
@@ -59,5 +64,60 @@ public class CustomerServiceUnitTests {
         assertNull(operationResult.errorMessage());
         assertNotNull(operationResult.createdObject());
         assertEquals("newcustomer@example.com", savedCustomer.getEmail());
+    }
+
+    @Test
+    void should_add_address_to_customer(){
+
+        Customer customer = new Customer();
+        customer.setCustomerId(1L);
+
+        when(customerRepository.findById(1l)).thenReturn(Optional.of(customer));
+
+        Address address = new Address();
+        address.setAddressId(2L);
+
+
+        when(addressRepository.findById(2L)).thenReturn(Optional.of(address));
+
+        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+
+        OperationResult result = customerService.addAddressToCustomer(1L, 2L);
+
+
+        assertTrue(result.success());
+        assertNull(result.errorMessage());
+        assertEquals(customer, result.createdObject());
+
+    }
+
+    @Test
+    void should_fail_when_adding_address_to_customer_and_Not_finding_customer(){
+
+        when(customerRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+
+        OperationResult operationResult = customerService.addAddressToCustomer(1L, 2L);
+
+        assertFalse(operationResult.success());
+        assertNull(operationResult.createdObject());
+        assertEquals(operationResult.errorMessage(),"Couldn't find customer with id " + 1L );
+
+
+
+    }
+
+    @Test
+    void should_fail_when_adding_address_to_customer_Not_finding_address(){
+
+        when(addressRepository.findById(2L)).thenReturn(Optional.empty());
+
+        Customer customer = new Customer();
+        when(customerRepository.findById(any(Long.class))).thenReturn(Optional.of(customer));
+
+        OperationResult result = customerService.addAddressToCustomer(1L, 2L);
+
+        assertFalse(result.success());
+        assertNull(result.createdObject());
+        assertEquals(result.errorMessage(),"Couldn't find address with id " + 2L );
     }
 }
